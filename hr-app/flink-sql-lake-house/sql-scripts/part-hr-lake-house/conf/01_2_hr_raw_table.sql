8553ffffc1a69f78a74c8122711fff0004b73219
gongzhiyong <@gongzhiyong>
2024-06-05 17:26:15 +0800

    sql提交

--- hr-app/flink-sql-lake-house/sql-scripts/part-hr-lake-house/conf/01_2_hr_raw_table.sql
+++ hr-app/flink-sql-lake-house/sql-scripts/part-hr-lake-house/conf/01_2_hr_raw_table.sql
@@ -1,11 +1,40 @@
-CREATE TABLE STREAM_JOB_MyUserTable (
-  id BIGINT,
-  name STRING,
-  age INT,
-  status BOOLEAN,
-  PRIMARY KEY (id) NOT ENFORCED
-) WITH (
-   'connector' = 'jdbc',
-   'url' = 'jdbc:mysql://localhost:3306/mydatabase',
-   'table-name' = 'users'
-);
+CREATE TABLE `RAW_HR_EMP_PROFRESULTPATENTINFO`
+(
+    `PROFID`              VARCHAR(2147483647) NOT NULL,
+    `EMPID`               VARCHAR(2147483647),
+    `RESULTNAME`          VARCHAR(2147483647),
+    `RESULTNO`            VARCHAR(2147483647),
+    `RESULTLV`            VARCHAR(2147483647),
+    `RESULTTYPE`          VARCHAR(2147483647),
+    `RESULTIDENLV`        VARCHAR(2147483647),
+    `RESULTIDENORG`       VARCHAR(2147483647),
+    `COMPLETERANK`        VARCHAR(2147483647),
+    `MAINCONTRIBUTION`    VARCHAR(2147483647),
+    `ROLE`                VARCHAR(2147483647),
+    `GAINTIME`            VARCHAR(2147483647),
+    `PERIOD`              VARCHAR(2147483647),
+    `WITNESS`             VARCHAR(2147483647),
+    `COMPLETEORG`         VARCHAR(2147483647),
+    `BENEFIT`             VARCHAR(2147483647),
+    `PATENTTYPE`          VARCHAR(2147483647),
+    `PATENTAPPCOUNTRY`    VARCHAR(2147483647),
+    `ISSUORG`             VARCHAR(2147483647),
+    `PROJECT`             VARCHAR(2147483647),
+    `OPTOR`               VARCHAR(2147483647),
+    `OPTTIME`             VARCHAR(2147483647),
+    `OPTORG`              VARCHAR(2147483647),
+    `STATUS`              VARCHAR(2147483647),
+    `NOTE`                VARCHAR(2147483647),
+    `PROJECTLEVEL`        VARCHAR(2147483647),
+    `PATENTUNIT`          VARCHAR(2147483647),
+    `GROUPPOST`           VARCHAR(2147483647),
+    PRIMARY KEY (`PROFID`) NOT ENFORCED
+)
+    WITH (
+        'connector'  = 'jdbc' ,
+        'driver'  = 'oracle.jdbc.driver.OracleDriver' ,
+        'url'  = 'jdbc:oracle:thin:@10.201.218.54:1521/hr' ,
+        'table-name'  = 'hr_emp_profresultpatentinfo'  ,
+        'username' ='hrorg_1217' ,
+        'password' ='rx5Hog'
+        );
